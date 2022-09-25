0ecd0c6db10eee0f8808b0a6502a627c1cf8b738
warriormaster12 <streng.alexander@outlook.com>
2022-09-25 19:51:57 +0300

    some work on converting to binary

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
+use std::u8;
 use std::vec::Vec;
 
 use std::fs::File;
@@ -10,6 +11,7 @@ use std::fs::File;
 use std::io::{self, Write};
 
 use std::path::Path;
+
  
 
 
@@ -18,6 +20,13 @@ struct MeshData {
     position:Vec<f32>,
     normals:Vec<f32>,
     color:Vec<f32>
+}
+
+struct AssetFile {
+    file_type: String,
+    version: i32,
+    json: String,
+    binary_blob: Vec<String>
 }
 
 fn input(i_txt: &str) -> String {
@@ -30,11 +39,49 @@ fn input(i_txt: &str) -> String {
     return input.trim().to_string();
 }
 
+
+fn save_binary(name: &str, file: AssetFile, custom_path:Option<&str>) -> bool{
+    let out_file_name:String;
+    if custom_path.is_some(){
+        out_file_name = custom_path.unwrap().to_string() + &name.to_string() + "." + &file.file_type 
+    }
+    else {
+        out_file_name = name.to_string() + "." + &file.file_type;
+    }
+    
+    let mut out_file = File::create(out_file_name).unwrap();
+    out_file.write(file.file_type.as_bytes()).unwrap();
+
+    //version
+    let version = file.version;
+    out_file.write(&version.to_ne_bytes()).unwrap();
+
+
+    //json length
+    let length = file.json.len();
+    out_file.write(&length.to_ne_bytes()).unwrap();
+    
+    //json stream
+    out_file.write(file.json.as_bytes()).unwrap();
+
+    //binary blob length
+    let blob_size = file.binary_blob.len();
+    out_file.write(&blob_size.to_ne_bytes()).unwrap();
+
+    //blob data 
+    for i in file.binary_blob{
+        out_file.write(i.as_bytes()).unwrap();
+    }
+
+    return true;
+}
+
 fn main() {
 
     let obj_file = input("select file to import: "); 
     //that's a long function
     let name = Path::new(&obj_file).file_stem().unwrap().to_os_string().into_string().unwrap();
+
 
     let (models, _materials) =
         tobj::load_obj(&obj_file, &tobj::LoadOptions::default()).expect("Failed to OBJ load file");
@@ -49,10 +96,8 @@ fn main() {
         };
     }
 
-    let json_file:String = name +".json";
-    let file = File::create(json_file.clone()).unwrap();
-    let serialized = serde_json::to_string(&mesh_data).unwrap();
-    serde_json::to_writer(file, &serialized).unwrap();
-    println!("generated: {}", json_file);
+    let binary_file:AssetFile = AssetFile{ file_type: "Mesh".to_string(), version: 1, json: serde_json::to_string(&mesh_data).unwrap(), binary_blob: [].to_vec() };
+
+    save_binary(&name, binary_file, Some("/home/alexander/ProgrammingProjects/Rust/FileConveretr-rs/samples/output/"));
     
 }
